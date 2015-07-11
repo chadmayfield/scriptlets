f2b6c1c0d1dde56fd60428e1cb3c94da7a1d5538
Chad Mayfield <code@chadmayfield.com>
2015-07-11 00:22:12 -0600

    sorted url by region and removed unused variable $url

--- bench_net.sh
+++ bench_net.sh
@@ -18,43 +18,49 @@ tmpfile="/tmp/$0.$$"
 options="-O /dev/null"
 
 declare -A array
+
 array["CacheFly CDN Network  "]="http://cachefly.cachefly.net/100mb.test"
-#array["Linode, Fremont CA USA"]="http://speedtest.fremont.linode.com/100MB-fremont.bin"
+
+#### NORTH AMERICA
+array["Linode, Fremont CA USA"]="http://speedtest.fremont.linode.com/100MB-fremont.bin"
+array["SoftLayer, SJ CA USA  "]="http://speedtest.sjc01.softlayer.com/downloads/test100.zip"
+array["SoftLayer, SEA WA USA "]="http://speedtest.sea01.softlayer.com/downloads/test100.zip"
 array["Linode, Dallas TX USA "]="http://speedtest.dallas.linode.com/100MB-dallas.bin"
 array["Linode, Atlanta GA USA"]="http://speedtest.atlanta.linode.com/100MB-atlanta.bin"
-#array["Linode, Newark NJ USA "]="http://speedtest.newark.linode.com/100MB-newark.bin"
-array["Linode, Tokyo JP      "]="http://speedtest.tokyo.linode.com/100MB-tokyo.bin"
-array["Linode, Singapore     "]="http://speedtest.singapore.linode.com/100MB-singapore.bin"
-#array["OVH, Beauharnois QC CA"]="http://bhs.proof.ovh.net/files/100Mb.dat"
-array["Leaseweb, Haarlem NL  "]="http://mirror.leaseweb.com/speedtest/100mb.bin"
-array["Bahnhof, Sundsvall SE "]="http://speedtest.bahnhof.se/100M.zip"
 array["DigitalOCean, NY USA  "]="http://speedtest-nyc1.digitalocean.com/100mb.test"
+array["Linode, Newark NJ USA "]="http://speedtest.newark.linode.com/100MB-newark.bin"
+array["SoftLayer, DC USA     "]="http://speedtest.wdc01.softlayer.com/downloads/test100.zip"
+array["OVH, Beauharnois QC CA"]="http://bhs.proof.ovh.net/files/100Mb.dat"
+
+#### EUROPE
+#array["Edis, Frankfurt DE    "]="http://de.edis.at/100MB.test"
+#array["Edis, Warsaw PL       "]="http://pl.edis.at/100MB.test"
+#array["DigitalOcean, AMS NL  "]="http://speedtest-ams1.digitalocean.com/100mb.test"
+#array["Leaseweb, Haarlem NL  "]="http://mirror.leaseweb.com/speedtest/100mb.bin"
+#array["Bahnhof, Sundsvall SE "]="http://speedtest.bahnhof.se/100M.zip"
 #array["DigitalOcean, LON EN  "]="http://ipv4.speedtest-lon1.digitalocean.com/100mb.test"
 #array["DigitalOcean, PAR FR  "]="http://speedtest-fra1.digitalocean.com/100mb.test"
-#array["DigitalOcean, AMS NL  "]="http://speedtest-ams1.digitalocean.com/100mb.test"
-#array["SoftLayer, Singapore  "]="http://speedtest.sng01.softlayer.com/downloads/test100.zip"
-#array["SoftLayer, SEA WA USA "]="http://speedtest.sea01.softlayer.com/downloads/test100.zip"
-array["SoftLayer, SJ CA USA  "]="http://speedtest.sjc01.softlayer.com/downloads/test100.zip"
-array["SoftLayer, DC USA     "]="http://speedtest.wdc01.softlayer.com/downloads/test100.zip"
-#array["Edis, Vina del Mar CL "]="http://cl.edis.at/100MB.test"
-#array["Edis, NTerritories HK "]="http://hk.edis.at/100MB.test"
-#array["Edis, Tel Aviv IL     "]="http://il.edis.at/100MB.test"
-#array["Edis, Moscow RU       "]="http://ru.edis.at/100MB.test"
 #array["Edis, Bucharest RO    "]="http://ro.edis.at/100MB.test"
 #array["Edis, Hafnarfjordur IS"]="http://is.edis.at/100MB.test"
-array["Edis, Frankfurt DE    "]="http://de.edis.at/100MB.test"
-#array["Edis, Warsaw PL       "]="http://pl.edis.at/100MB.test"
+#array["Edis, Moscow RU       "]="http://ru.edis.at/100MB.test"
+#array["Edis, Tel Aviv IL     "]="http://il.edis.at/100MB.test"
+
+#### SOUTH AMERICA
+#array["Edis, Vina del Mar CL "]="http://cl.edis.at/100MB.test"
+
+#### ASIA/PACIFICA
+#array["Linode, Tokyo JP      "]="http://speedtest.tokyo.linode.com/100MB-tokyo.bin"
+#array["Linode, Singapore     "]="http://speedtest.singapore.linode.com/100MB-singapore.bin"
+#array["SoftLayer, Singapore  "]="http://speedtest.sng01.softlayer.com/downloads/test100.zip"
+#array["Edis, NwTerritories HK"]="http://hk.edis.at/100MB.test"
 
 echo "beginning speed/latency tests..."
 echo "  start time: $(date)"
 
 for i in "${!array[@]}"
 do
-    # set our url
-    url=${array[$i]}
-
     # run wget as out speedtest & save the output
-    wget $options $url &> $tmpfile
+    wget $options ${array[$i]} &> $tmpfile
     speed=$(awk '/\/dev\/null/ {s=$3 $4} END {gsub(/\(|\)/,"",s); print s}' $tmpfile)
     
     # find a quick avg latency (does work with wget >1.13)
@@ -62,7 +68,7 @@ do
     cmd=$(ping -q -c 20 -i 0.2 -w 5 $ip | \
           awk -F "/" '/rtt/ {print $5 " ms latency"}')
 
-    echo -ne "  Speed from $i   :  ${speed}\t (${cmd})\n"
+    echo -ne "  Speed from $i : ${speed}\t(${cmd})\n"
 done
 
 echo "  end time: $(date)"
