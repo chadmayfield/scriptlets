2623de7a15f999c0d12dabd3724361a6374df4b4
Chad Mayfield <chad@Chads-MBP.local>
2017-03-29 18:53:14 -0600

    simple script to update all vagrant boxes found in current directory

---
+++ vagrant_update_boxes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# vagrant_update_boxes.sh - update all vagrant boxes under current directory
+
+for i in $(find . -name Vagrantfile)
+do
+    echo "Found Vagrantfile at: $i"
+    cd $(echo $i | sed 's/Vagrantfile//')
+
+    boxname=$(grep "^  config.vm.box " Vagrantfile | awk -F "= " '{print $2}')
+    echo "Updating box: $boxname"
+
+    vagrant box update
+
+    cd - &> /dev/null
+done
+
+#EOF
