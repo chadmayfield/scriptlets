fa418b1f72583199700b465997d92f45859c4e36
Chad Mayfield <chad@chd.my>
2017-04-07 19:27:28 -0600

    Huge upgrade to use printf rather than echo to format nicely, added a bunch of features; network tx/rx, established connections, docker version & running containers, hardware information like serial number and UUID

--- README.md
+++ README.md
@@ -74,50 +74,76 @@ test/Using Docker.pdf -> test/Using.Docker.pdf
 test/Version Control with Git Second Edition.pdf -> test/Version.Control.with.Git.Second.Edition.pdf
 ```
 
-* **sysinfo.sh** - show system information for various oses
+* **sysinfo.sh** - show system information for various oses, including load, uptime, cpu information, docker version and running containers, and network information. When used with the `--connections` option it will show all established connections with hostname if possible.
+
+(As of now works with macOS Sierra, RHEL, CentOS, Ubuntu, Debian)
 
 Apple macOS Sierra
 ```
-macbookpro:~ $ ./cpuinfo.sh 
------------------------------------
-Current System Information Snapshot
------------------------------------
-Current date: Thu Apr  6 13:10:32 MDT 2017
-Hostname: macbookpro.lan
-  Uptime: 2 days  2:49 hours
-  Load average: 1.05 1.27 1.27
-  Kernel: Darwin 16.5.0
-Prcoessor Information: Intel(R) Core(TM) i5-6360U CPU @ 2.00GHz
-  Processor Cores: 2
-  Virtual Processors: 4
-Memory Information: 8.00 gigabytes
-  Memory stats: 7088M used (1684M wired), 1101M unused.
-Network Information 
-  Internal Address: 192.168.1.22
-  External Address: 206.170.114.132 (mobile-206-170-114-132.myvzw.net)
------------------------------------
+macbookpro:~ $ ./sysinfo.sh --connections 
+------------------------------------------------------------------------
+Current Date:        Fri Apr  7 18:59:43 MDT 2017
+Hostname:            macbookpro.local
+OS:                  Mac OS X 10.12.4 (16E195)
+Kernel:              Darwin 16.5.0
+HW Version:          MacBook Pro (13-inch, 2016, Two Thunderbolt 3 ports)
+HW Serial:           8SF79S7DA9SF
+HW UUID:             6EB1244E-BC1F-323A-9724-5AB7B382D921
+Uptime:              21:01 hours
+Load Average:        1.34 1.37 1.28
+Processor:           Intel(R) Core(TM) i5-6360U CPU @ 2.00GHz
+Core Count:          2
+Virtual Cores:       4
+Total Memory:        8.00 gigabytes
+Memory Used:         7956M used (1864M wired), 234M unused.
+Internal IP:         192.168.1.213 (Tx/Rx:  bytes)
+External IP:         166.72.33.98 (166-72-33-98.xmission.com)
+Docker Version:      17.03.1-ce
+Containers:          CONTAINER ID       NAME 
+                     b22c0ef58f62	confident_bose
+                     50b7deb5898f	loving_torvalds
+Current Connections: 192.168.110.213
+                     173.194.196.109 (ix-in-f109.1e100.net.)
+                     34.192.55.128 (ec2-34-192-55-128.compute-1.amazonaws.com.)
+                     40.97.150.242
+                     52.2.45.245 (ec2-52-2-45-245.compute-1.amazonaws.com.)
+                     54.84.82.15 (ec2-54-84-82-15.compute-1.amazonaws.com.)
+                     65.52.108.204 (msnbot-65-52-108-204.search.msn.com.)
+                     74.125.202.108 (io-in-f108.1e100.net.)
+------------------------------------------------------------------------
 ``` 
 
 Red Hat Enterprise Linux 7.3
 ```
-[chad@fileserver ~]$ ./cpuinfo.sh 
------------------------------------
-Current System Information Snapshot
------------------------------------
-Current date: Thu Apr  6 19:19:48 UTC 2017
-Hostname: fileserver.localhost
-  Uptime: 28 days  1:12 hours
-  Load average: 3.96, 1.48, 0.84
-  Kernel: Linux 3.10.0-514.10.2.el7.x86_64
-CPU Information: Intel(R) Xeon(R) CPU E3-1245 v3 @ 3.40GHz
-  Processor Cores: 4
-  Virtual Processors: 8
-Memory Information: 23.17 gigabytes
-  Memory stats: 1.2G used (of 23G), 20.8G unused.
-Network Information 
-  Internal Address: 192.168.1.251
-  External Address: 161.170.14.11 (161-170-014-011.xmission.com)
------------------------------------
+[chad@fileserver ~]$ ./sysinfo.sh --connections 
+[root@file chad]# ./sysinfo.sh --connections
+------------------------------------------------------------------------
+Current Date:        Fri Apr  7 18:57:35 MDT 2017
+Hostname:            fileserver.local
+OS:                  Red Hat Enterprise Linux Server release 7.3 (Maipo)
+Kernel:              Linux 3.10.0-514.10.2.el7.x86_64
+HW Version:          Dell Inc. PowerEdge T20
+HW Serial:           None
+HW UUID:             4C4C6544-0082-3610-8056-B4C04FY05A21
+Uptime:              29 days hours
+Load Average:        0.00, 0.02, 0.05
+Processor:           Intel(R) Xeon(R) CPU E3-1245 v3 @ 3.40GHz
+Core Count:          4
+Virtual Cores:       8
+Total Memory:        23.30 gigabytes
+Memory Used:         Mem: used (of 23G), 1.1G unused. 
+Internal IP:         192.168.110.30 (Tx/Rx: 121.3 GiB)
+External IP:         166.72.33.98 (166-72-33-98.xmission.com)
+Docker Version:      1.12.6
+Containers:          CONTAINER ID       NAME 
+                     d4199824e0a5	plex
+                     1b92338bbb80	unifi
+Current Connections: 192.168.110.3
+                     52.41.5.6 (ec2-52-41-5-6.us-west-2.compute.amazonaws.com.)
+                     52.41.5.6 (ec2-52-41-5-6.us-west-2.compute.amazonaws.com.)
+                     52.41.5.6 (ec2-52-41-5-6.us-west-2.compute.amazonaws.com.)
+                     192.168.110.213
+------------------------------------------------------------------------
 ```
 
 
--- sysinfo.sh
+++ sysinfo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 
 # sysinfo.sh - get hw stats for differnet oses
 
@@ -7,72 +7,192 @@
 # date    : 04/19/2009 (updated 04/05/2017)
 
 # TODO:
-#   + Add Network Tx/Rx Totals
 #   + Add Drive information
-#   + Add USB device information
+#   + Fix uptime to display entire uptime
+#   + Parameterize better
 
-b=$(tput bold)
-n=$(tput sgr0)
+if [[ $1 =~ "help" ]]; then
+    echo "$0, displays basic system information, such as uptime, load, & IP's."
+    echo "  e.g. $0"
+    echo ""
+    echo "To view info AND established connections;"
+    echo "  $0 --connections"
+    exit 1
+fi
 
+# grab some basic information
 currdate=$(date)
-up=$(uptime | awk 'BEGIN{FS="up |,"}{print $2$3" hours"}')
 kernel=$(uname -sr)
-command curl -s http://ipinfo.io > /tmp/ext_ip
-int_ip=$(ifconfig | grep "inet.*broadcast" | awk '{print $2}')
+up=$(uptime | awk 'BEGIN{FS="up |,"}{print $2" hours"}')
+
+# check if docker is installed
+command -v docker >/dev/null 2>&1; has_docker=1 || { has_docker=0; }
+
+# check if ifconfig is installed
+command -v ifconfig >/dev/null 2>&1 || \
+    { echo >&2 "ERROR: I require 'ifconfig' but it's not installed!"; exit 1; }
+
+# if docker is installed set some variables
+if [ $has_docker -eq 1 ]; then
+    if [[ $OSTYPE =~ "linux" ]]; then
+        docip=$(ifconfig docker0 | grep "inet " | awk '{print $2}' |grep -v ":")
+        int_ip=$(ifconfig | grep "inet.*broadcast" | grep -v "$docip" | \
+                 awk '{print $2}')
+    else
+        int_ip=$(ifconfig | grep "inet.*broadcast" | awk '{print $2}')
+    fi
+
+    docver=$(docker version |grep -A1 Server | awk '/Version/ {print $2}')
+    running=$(docker ps | grep -v CONTAINER | awk '{print $1"|"$NF}')
+else
+    int_ip=$(ifconfig | grep "inet.*broadcast" | awk '{print $2}')
+fi
+
+# get external ip address
+command curl -s http://ipinfo.io > /tmp/ext_ip || \
+    { echo >&2 "ERROR: You must be connected to the internet!"; exit 1; }
 ext_ip=$(grep ip /tmp/ext_ip | awk '{gsub(/[",]/, ""); print $2}')
 ext_hn=$(grep hostname /tmp/ext_ip | awk '{gsub(/[",]/, ""); print $2}')
 
+# set variables depending on os (between macOS & Ubuntu/Debian/RHEL)
 if [[ $OSTYPE =~ "darwin" ]]; then
-    load=$(uptime | awk 'BEGIN{FS="averages:"}{print $2}')
+    # get some OS/HW specific information
+    os_name=$(sw_vers -productName)
+    os_version=$(sw_vers -productVersion)
+    os_build=$(sw_vers -buildVersion)
+    serial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+    # get hw version: http://apple.stackexchange.com/a/98089
+    hw=$(curl -s http://support-sp.apple.com/sp/product?cc=$(echo $serial | \
+         cut -c 9-) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|')
+    uuid=$(system_profiler SPHardwareDataType | awk '/UUID/ { print $3 }')
+
+    # get load stats
+    load=$(uptime | awk 'BEGIN{FS="averages:"}{print $2}' | cut -c2-)
+
+    # get cpu info
     cpu_model=$(sysctl -n machdep.cpu.brand_string)
     physical_cpus=$(sysctl -n hw.physicalcpu)
     cores=$(sysctl -n machdep.cpu.core_count)
     virt_cores=$(sysctl -n hw.ncpu)
-    proc_flags=$(sysctl -n machdep.cpu.extfeatures | fold -w 40)
-    proc_flags+=$(sysctl -n machdep.cpu.leaf7_features | fold -w 60)
+    proc_flags=$(sysctl -n machdep.cpu.extfeatures)
+    proc_flags+=$(sysctl -n machdep.cpu.leaf7_features)
+
+    # get memory info
     ttl_mem=$(hostinfo | awk '/available:/ {print $4" "$5}')
     ### serveral other ways to get total memory on macos
     #ttl_mem=$(sysctl hw.memsize | awk 'print $2')
     #ttl_mem=$(ps -caxm -orss= | awk '{ sum += $1 } END { print sum/1024 }')
     mem=$(top -l 1 -s 0 | grep PhysMem | awk -F ': ' '{print $2}') 
+
+    # additional network information
+    txrx="$(netstat -ib -I en0 | grep -i $(hostname) | awk '{print $10}') bytes"
+    conn=$(netstat -anf inet | awk '{print $5}' | grep [0-9] | \
+       grep -vE 'x|127.0.0.1' | awk -F . '{print $1"."$2"."$3"."$4}' |sort -u)
+
+    # get drive/mount point information
 #    drives=$(diskutil list)
 elif [[ $OSTYPE =~ "linux" ]]; then
+    # get some OS/HW specific information
+    if [ -f /etc/redhat-release ]; then
+        os_version=$(cat /etc/redhat-release)
+    else
+        os_version=$(lsb_release -a |awk '/Desc:/ {first=$1; $1=""; print $0}')
+    fi
+
+    serial="None"
+
+    # test whether user can use dmidecode
+    a=$( { dmidecode -q > /tmp/dmitest; rm -f /tmp/dmitest; } 2>&1)
+    if [[ $a =~ "denied" ]]; then
+        hw=">>>FOR THIS STAT, RUN AS ROOT<<<"
+        uuid=">>>FOR THIS STAT, RUN AS ROOT<<<"
+    else
+        hw=$(dmidecode --type system | grep -E 'Manuf|Product'|\
+             awk -F ": " '{print $2}' | paste -sd ' ' -)
+        uuid=$(dmidecode | grep "UUID:" | awk '{print $2}')
+    fi
+
+    # get load stats
     loc="/proc/cpuinfo"
-    load=$(uptime | awk 'BEGIN{FS="average:"}{print $2}')
+    load=$(uptime | awk 'BEGIN{FS="average:"}{print $2}'| cut -c2-)
+
+    # get cpu info
     cpu_model=$(grep '^model name' $loc | uniq | awk -F ": " '{print $2}')
     physical_cpus=$(grep 'physical id' $loc | sort | uniq | wc -l)
     cores=$(grep 'cpu cores' $loc | uniq | awk 'BEGIN{FS=": "}{print $2}')
     virt_cores=$(grep ^processor $loc | wc -l)
     proc_flags=$(grep 'flags' $loc | uniq | awk 'BEGIN{FS=": "}{print $2}')
-    ttl_mem=$(free -tm | grep Mem: | awk 'printf "%.2f gigabytes\n,$2/1024"')
+
+    # get memory info
+    ttl_mem=$(free -tm | grep Mem: | awk '{printf "%.2f gigabytes\n",$2/1024}')
     mem=$(free -th |grep Mem:|awk '{print $1" used (of "$2"), "$3" unused. "}')
+
+    # additional network information
+    txrx=$(ifconfig | grep -A3 $int_ip | awk '/RX/ {print $6" "$7}' | \
+            sed -e 's/[()]//g')
+    conn=$(netstat -nat | awk '{print $5}' | grep [0-9] | \
+           grep -vE 'x|127.0.0.1|0.0.0.0' | awk -F ":" '{print $1}')
+
+    # get drive/mount point information
 #    drives=$(fdisk -l)
 else
     echo "ERROR: Unknown \$OSTYPE!"
     exit 1
 fi
 
-echo "-----------------------------------"
-echo "Current System Information Snapshot"
-echo "-----------------------------------"
-echo "${b}Current date:${n} $currdate"
-echo "${b}Hostname:${n} $(hostname)"
-echo "  ${b}Uptime:${n} $up"
-echo "  ${b}Load average:${n}${load}"
-echo "  ${b}Kernel:${n} $kernel"
-echo "${b}Prcoessor Information:${n} $cpu_model"
-echo "  ${b}Processor Cores:${n} $cores"
-echo "  ${b}Virtual Processors:${n} $virt_cores"
-#echo "  ${b}Processor Flags:${n} $proc_flags"
-echo "${b}Memory Information:${n} $ttl_mem"
-echo "  ${b}Memory stats:${n} $mem"
-echo "${b}Network Information${n} "
-echo "  ${b}Internal Address:${n} $int_ip"
-echo "  ${b}External Address:${n} $ext_ip (${ext_hn})"
-#echo "  ${b}Total Tx/Rx$:{n} "
-#echo "${b}Drive Information${n} "
-#echo "$drives"
-echo "-----------------------------------"
+echo "------------------------------------------------------------------------"
+printf "%-20s %s\n" "Current Date:" "$currdate" 
+printf "%-20s %s\n" "Hostname:" "$(hostname)"
+
+if [[ $OSTYPE =~ "darwin" ]]; then
+    printf "%-20s %s %s (%s)\n" "OS:" "$os_name" "$os_version" "$os_build"
+else
+    printf "%-20s %s\n" "OS:" "$os_version"
+fi
+
+printf "%-20s %s\n" "Kernel:" "$kernel"
+printf "%-20s %s\n" "HW Version:" "$hw"
+printf "%-20s %s\n" "HW Serial:" "$serial"
+printf "%-20s %s\n" "HW UUID:" "$uuid"
+printf "%-20s %s\n" "Uptime:" "$up"
+printf "%-20s %s\n" "Load Average:" "$load"
+printf "%-20s %s\n" "Processor:" "$cpu_model"
+printf "%-20s %s\n" "Core Count:" "$cores"
+printf "%-20s %s\n" "Virtual Cores:" "$virt_cores"
+#printf "%-20s %s\n" "Flags:" "$flags"
+printf "%-20s %s\n" "Total Memory:" "$ttl_mem"
+printf "%-20s %s\n" "Memory Used:" "$mem"
+printf "%-20s %s (Tx/Rx: %s)\n" "Internal IP:" "$int_ip" "$txrx"
+printf "%-20s %s (%s)\n" "External IP:" "$ext_ip" "$ext_hn"
+
+# if docker is install, print stats
+if [ $has_docker -eq 1 ]; then
+    printf "%-20s %s\n" "Docker Version:" "$docver"
+
+    if [ $(docker ps | grep -v IMAGE | wc -l) -ne 0 ]; then
+        printf "%-20s %s %s\n" "Containers:" "CONTAINER ID       NAME"
+        for i in ${running[@]}
+        do
+            printf "%-20s %s\n" "" "$(echo $i | awk -F"|" '{print $1"\t"$2}')"
+        done
+    fi
+fi
+
+# print current connections is arg is connections
+if [[ $1 =~ "connections" ]]; then
+    printf "%-20s %s\n" "Current Connections:" "$int_ip"
+    for i in ${conn[@]}
+    do
+        rdns=$(host $i | awk '{print $NF}' | uniq)
+        if [[ $rdns =~ "NXDOMAIN" ]]; then
+            printf "%-20s %s\n" " " "$i"
+        else
+            printf "%-20s %s (%s)\n" " " "$i" "$rdns"
+        fi
+    done
+fi
+
+echo "------------------------------------------------------------------------"
 
 rm -f /tmp/ext_ip
 
