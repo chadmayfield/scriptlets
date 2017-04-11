86e1b0265dc9552c8cc50400a4022cfe29399d1c
Chad Mayfield <chad@chd.my>
2017-04-10 21:21:39 -0600

    added macgen from http://red.ht/2pnu0Xy

---
+++ macgen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# macgen.py script to generate a MAC address for vm http://red.ht/2pnu0Xy
+
+# TODO: extend functionality
+
+import random
+
+def randomMAC():
+	mac = [ 0x00, 0x16, 0x3e,
+		random.randint(0x00, 0x7f),
+		random.randint(0x00, 0xff),
+		random.randint(0x00, 0xff) ]
+	return ':'.join(map(lambda x: "%02x" % x, mac))
+#
+print randomMAC()
