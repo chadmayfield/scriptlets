d50143446cbd12f8166cbcf4c0e6b4944db47303
Chad Mayfield <chad@chd.my>
2018-06-11 19:24:28 -0600

    updated myrepo pull to add keys

--- myrepos_update.sh
+++ myrepos_update.sh
@@ -2,41 +2,69 @@
 
 # update_myrepos.sh - update all my repos under the current tree
 
-if [ $(ps -ef | grep " ssh-agen[t]" | wc -l) -eq 0 ]; then
-    echo "ssh-agent is not running, starting it..."
+keys=( "$HOME/.ssh/github.com/id_ed25519" ) 
 
-    # easy way to grab failure without set -e set
-    # http://stackoverflow.com/a/14389672
-    if eval $(ssh-agent -s); then
-        # success
-        :
-    else
-        # failure
-        echo "start failed!" && exit 1
-    fi
+bold=$(tput bold)       
+normal=$(tput sgr0)
+black=$(tput setaf 0)    # COLOR_BLACK/RGB:0,0,0
+red=$(tput setaf 1)      # COLOR_RED/RGB:255,0,0
+green=$(tput setaf 2)    # COLOR_GREEN/RGB:0,255,0
+yellow=$(tput setaf 3)   # COLOR_YELLOW/RGB:255,255,0
+blue=$(tput setaf 4)     # COLOR_BLUE/RGB:0,0,255
+magenta=$(tput setaf 5)  # COLOR_MAGENTA/RGB:255,0,255
+cyan=$(tput setaf 6)     # COLOR_CYAN/RGB:0,255,255
+white=$(tput setaf 7)    # COLOR_WHITE/RGB:255,255,255
+
+# check if ssh-agent is running & start it
+if [ $(ps -A | grep "[s]sh-agent -s" | wc -l) -eq 0 ]; then
+    echo "ssh-agent is not running!"
+    echo "starting..."
+    eval "$(ssh-agent -s)"
+else
+    echo "ssh-agent is already running."
 fi
 
-if [ $(ssh-add -l | grep -v "no identities" | wc -l) -eq 0 ]; then
-    echo "no ssh key found in agent! add it with ssh-add;"
-    echo "  e.g. ssh-add ~/.ssh/id_rsa"
-    exit 1
-fi
+echo "checking for key..."
+for i in ${keys[@]}; do
+	# grab key fingerprint
+    cmp_key=$(ssh-keygen -lf $i)
+	
+	# if key fingerprint not found in fingerprint list, add it
+	if [ $(ssh-add -l | grep -c "$cmp_key") -eq 0 ]; then
+    	echo "key not found."
+    	echo "adding key..."
+		ssh-add $i
+	else
+    	echo "key found."
+	fi
+done
 
-# git pull all repositories in tree
-for i in $(ls -d */)
+# iterate through all child dirs to find git repos
+echo "pulling updates..."
+for i in $(find $(pwd) -name .git | sed 's/\/.git//g' | sort)
 do
-    cd $i
+	(
+    cd "$i"
 
     if [ -e .git ]; then
         repo=$(git config --local -l | grep "remote.origin.url" | awk -F "=" '{print $2}')
-        echo "Found repo: $repo"
+        echo " "
+
+		if [[ $repo =~ "@" ]]; then
+            repotype="SSH"
+		else
+			repotype="HTTPS"
+		fi
+
+        echo "====================================================="
+        echo "${bold}Found repo ($repotype): ${yellow}$repo${normal}"
         echo "Pulling latest changes..."
         git pull
+		git pull --tags
     else
         :
     fi
-
-    cd ..
+    )
 done
 
 #EOF
