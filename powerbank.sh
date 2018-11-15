80bc332d215b10c10acd794ad7d008498a3b4125
Chad Mayfield <chad@chd.my>
2018-11-15 13:24:48 -0700

    added placeholder for powerbank script

---
+++ powerbank.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# powerbank.sh - calculate number of charges of a powerbank
+
+# author  : Chad Mayfield (chad@chd.my)
+# license : gplv3
+
+# TODO
+#   + write the script!
+#   algo: (labeled powerbank capacity x 3.7 / output voltage) * 0.85 / pb capacity / capacity of device = # recharges 
+#   example: (16000 x 3.7 / 5) x 0.85 / 1821 = 5.5 charges
+
+
+
+
+
+
+
+
+#EOF
